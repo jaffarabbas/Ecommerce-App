36d05c1277b49817eeb9cc5a18338a0f4f3a1082
jaffarabbas <gamaportal8@gmail.com>
2023-08-23 23:26:37 +0500

    Commited File : app-routing.module.ts at 23/08/2023 11:26:37 pm

--- EcommerceAppInterface/src/app/app-routing.module.ts
+++ EcommerceAppInterface/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { AuthComponent } from './modules/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch: 'full'},
+  {path:'home',component:HomeComponent,loadChildren:()=>import('./modules/home/home.module').then(m=>m.HomeModule)},
+  {path:'auth',component:AuthComponent,loadChildren:()=>import('./modules/auth/auth.module').then(m=>m.AuthModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
