2f46c7542cbb233a44dd8512a2e6b43961b6f03f
jaffarabbas <gamaportal8@gmail.com>
2023-08-23 23:26:42 +0500

    Commited File : home.module.ts at 23/08/2023 11:26:42 pm

--- EcommerceAppInterface/src/app/modules/home/home.module.ts
+++ EcommerceAppInterface/src/app/modules/home/home.module.ts
@@ -3,15 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { IndexComponent } from './components/index/index.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SharedComponentsModule } from 'src/app/shared/shared-components/shared-components.module';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    IndexComponent,
+    AboutComponent,
+    ContactComponent,
+    BannerComponent,
+    ProductsComponent,
+    CategoriesComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedComponentsModule
   ]
 })
 export class HomeModule { }
