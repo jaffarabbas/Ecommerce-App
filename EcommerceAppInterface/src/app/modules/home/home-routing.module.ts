fbdc417bbd368d9a60a5a57dcafb0e527da02f3d
jaffarabbas <gamaportal8@gmail.com>
2023-08-23 23:26:41 +0500

    Commited File : home-routing.module.ts at 23/08/2023 11:26:41 pm

--- EcommerceAppInterface/src/app/modules/home/home-routing.module.ts
+++ EcommerceAppInterface/src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'index',pathMatch: 'full'},
+  {path:'index',component:IndexComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
