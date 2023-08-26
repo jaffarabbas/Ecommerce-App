8b74040f562c491c93475dd1c3eabc9b1741f16a
jaffarabbas <gamaportal8@gmail.com>
2023-08-27 00:01:54 +0500

    Commited File : products.component.ts at 27/08/2023 12:01:54 am

--- EcommerceAppInterface/src/app/modules/home/components/products/products.component.ts
+++ EcommerceAppInterface/src/app/modules/home/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ProductCard} from "../../../../interfaces/ProductCard";
 
 @Component({
   selector: 'app-products',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
-
+  ListProducts:ProductCard[] = [
+    {
+      id: 1,
+      name: 'Product 1',
+      imageUrl: '../../../../../assets/images/productPic1.png'
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      imageUrl: '../../../../../assets/images/productPic2.png'
+    },{
+      id: 3,
+      name: 'Product 3',
+      imageUrl: '../../../../../assets/images/productPic3.png'
+    },{
+      id: 4,
+      name: 'Product 4',
+      imageUrl: '../../../../../assets/images/productPic4.png'
+    }
+  ];
 }
