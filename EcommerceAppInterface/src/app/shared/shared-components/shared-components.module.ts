f97259b2cf541f895429a696328b55397b374e7e
jaffarabbas <gamaportal8@gmail.com>
2023-08-27 00:01:59 +0500

    Commited File : shared-components.module.ts at 27/08/2023 12:01:59 am

--- EcommerceAppInterface/src/app/shared/shared-components/shared-components.module.ts
+++ EcommerceAppInterface/src/app/shared/shared-components/shared-components.module.ts
@@ -2,20 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import {RouterLink} from "@angular/router";
+import { CustomBannerComponent } from './custom-banner/custom-banner.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CustomBannerComponent,
+    ProductCardComponent
   ],
-  imports: [
-    CommonModule
-  ],
-  exports:[
+    imports: [
+        CommonModule,
+        RouterLink
+    ],
+  exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CustomBannerComponent,
+    ProductCardComponent
   ]
 })
 export class SharedComponentsModule { }
