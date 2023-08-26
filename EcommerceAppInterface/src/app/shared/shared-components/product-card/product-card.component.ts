39febba9e313bb87faa3f0aa7038cc457ebde1ae
jaffarabbas <gamaportal8@gmail.com>
2023-08-27 00:01:41 +0500

    Commited File : product-card.component.ts at 27/08/2023 12:01:41 am

---
+++ EcommerceAppInterface/src/app/shared/shared-components/product-card/product-card.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+import {ProductCard} from "../../../interfaces/ProductCard";
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss']
+})
+export class ProductCardComponent {
+  @Input() product!:ProductCard;
+}
