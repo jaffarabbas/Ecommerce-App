716af6730c39e2066b057caa0a54facc8f479ee2
jaffarabbas <gamaportal8@gmail.com>
2023-08-27 00:01:56 +0500

    Commited File : shop-routing.module.ts at 27/08/2023 12:01:56 am

--- EcommerceAppInterface/src/app/modules/shop/shop-routing.module.ts
+++ EcommerceAppInterface/src/app/modules/shop/shop-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'shop',pathMatch:'full'},
+  {path:'shop',component:ShopComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
