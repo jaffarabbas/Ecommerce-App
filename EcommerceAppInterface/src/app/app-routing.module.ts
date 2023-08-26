26e8800f9c569023e70ca345805744b9285f5803
jaffarabbas <gamaportal8@gmail.com>
2023-08-27 00:01:42 +0500

    Commited File : app-routing.module.ts at 27/08/2023 12:01:42 am

--- EcommerceAppInterface/src/app/app-routing.module.ts
+++ EcommerceAppInterface/src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { AuthComponent } from './modules/auth/auth.component';
+import { CartComponent } from './modules/cart/cart.component';
+import { ShopComponent } from './modules/shop/shop.component';
+import { CheckoutComponent } from './modules/checkout/checkout.component';
+import { PaymentComponent } from './modules/payment/payment.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch: 'full'},
-  {path:'home',component:HomeComponent,loadChildren:()=>import('./modules/home/home.module').then(m=>m.HomeModule)},
-  {path:'auth',component:AuthComponent,loadChildren:()=>import('./modules/auth/auth.module').then(m=>m.AuthModule)},
+  {path:'home', component:HomeComponent,loadChildren:()=>import('./modules/home/home.module').then(m=>m.HomeModule)},
+  {path:'auth', component:AuthComponent,loadChildren:()=>import('./modules/auth/auth.module').then(m=>m.AuthModule)},
+  {path:'cart', component:CartComponent, loadChildren:()=>import('./modules/cart/cart.module').then(m=>m.CartModule)},
+  {path:'shop', component:ShopComponent, loadChildren:()=>import('./modules/shop/shop.module').then(m=>m.ShopModule)},
+  {path:'checkout', component:CheckoutComponent ,loadChildren:()=>import('./modules/checkout/checkout.module').then(m=>m.CheckoutModule)},
+  {path:'payment', component:PaymentComponent ,loadChildren:()=>import('./modules/payment/payment.module').then(m=>m.PaymentModule)},
 ];
 
 @NgModule({
