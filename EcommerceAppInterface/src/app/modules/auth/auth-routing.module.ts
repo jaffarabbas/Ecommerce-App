2539099260bc1207a0b62a39745aa56d9ffb9471
jaffarabbas <gamaportal8@gmail.com>
2023-08-27 19:28:40 +0500

    Commited File : auth-routing.module.ts at 27/08/2023 7:28:40 pm

--- EcommerceAppInterface/src/app/modules/auth/auth-routing.module.ts
+++ EcommerceAppInterface/src/app/modules/auth/auth-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { TermsAndConditionsComponent } from './components/terms-and-conditions/terms-and-conditions.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'login',pathMatch: 'full'},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
   {path:'forget-password/:id',component:ForgetPasswordComponent},
+  {path:'termsandconditions',component:TermsAndConditionsComponent},
 ];
 
 @NgModule({
