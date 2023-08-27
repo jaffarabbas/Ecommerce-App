17a1989034c707c4d0822c477661846a0ba18d18
jaffarabbas <gamaportal8@gmail.com>
2023-08-27 19:28:39 +0500

    Commited File : custom-submit.component.ts at 27/08/2023 7:28:39 pm

---
+++ EcommerceAppInterface/src/app/shared/shared-components/custom-submit/custom-submit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-submit',
+  templateUrl: './custom-submit.component.html',
+  styleUrls: ['./custom-submit.component.scss']
+})
+export class CustomSubmitComponent {
+
+}
