ec49dd1e411f2d03b227310c0043639862ca7eaf
jaffarabbas <gamaportal8@gmail.com>
2023-09-02 22:11:06 +0500

    Commited File : login.component.ts at 02/09/2023 10:11:05 pm

--- EcommerceAppInterface/src/app/modules/auth/components/login/login.component.ts
+++ EcommerceAppInterface/src/app/modules/auth/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ErrorMessageService} from "../../../../services/core/error-message.service";
+import {ErrorMessages} from "../../../../utilities/constants/errorMessages";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,22 @@ import { Component, Input } from '@angular/core';
 })
 export class LoginComponent {
   @Input() isShowBanner: boolean = true;
+  submitted: boolean = false;
+
+  constructor(public errorHandler:ErrorMessageService) {
+  }
+
+  formGroup:FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  submit(){
+    this.submitted = true;
+    if(this.formGroup.valid){
+      console.log(this.formGroup.value);
+    }
+  }
+
+  protected readonly ErrorMessages = ErrorMessages;
 }
