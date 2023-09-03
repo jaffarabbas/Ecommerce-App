3f68cdd935bdef42979eec05dfc50e28a2297d14
jaffarabbas <gamaportal8@gmail.com>
2023-09-03 16:28:06 +0500

    Commited File : errorMessages.ts at 03/09/2023 4:28:06 pm

--- EcommerceAppInterface/src/app/utilities/constants/errorMessages.ts
+++ EcommerceAppInterface/src/app/utilities/constants/errorMessages.ts
@@ -6,11 +6,18 @@ export class ErrorMessages{
       controlName: "email",
       errorName: "required",
       message: "Email is required",
-    },
-    {
+    },{
       controlName: "email",
       errorName: "email",
       message: "Email is invalid",
+    },{
+      controlName: "password",
+      errorName: "required",
+      message: "Password is required",
+    },{
+      controlName: "password",
+      errorName: "minlength",
+      message: "Password must be at least 8 characters",
     }
   ]
 }
