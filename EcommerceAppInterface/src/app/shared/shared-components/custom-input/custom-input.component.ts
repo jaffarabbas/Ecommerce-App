d88b18974d31a8f1ee33b4080283f9e4a2d66a30
jaffarabbas <gamaportal8@gmail.com>
2023-09-03 16:28:01 +0500

    Commited File : custom-input.component.ts at 03/09/2023 4:28:01 pm

---
+++ EcommerceAppInterface/src/app/shared/shared-components/custom-input/custom-input.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-custom-input',
+  templateUrl: './custom-input.component.html',
+  styleUrls: ['./custom-input.component.scss']
+})
+export class CustomInputComponent {
+  @Input() class!: string;
+  @Input() formGroup!: FormGroup;
+  @Input() controlName!: string;
+  @Input() type: string = "text";
+  @Input() placeholder: string = "";
+  @Input() label: string = "";
+  @Input() isRequired: boolean = false;
+  @Input() errorMessage:string = "";
+
+  detectChanges(event: any) {
+    if (event.target.value.length > 0) {
+      this.errorMessage = "";
+    }
+  }
+}
