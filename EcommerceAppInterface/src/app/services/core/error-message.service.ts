ea964c60b5d50ef13be575ba2147b71d9c7e0417
jaffarabbas <gamaportal8@gmail.com>
2023-09-10 01:34:32 +0500

    Commited File : error-message.service.ts at 10/09/2023 1:34:31 am

--- EcommerceAppInterface/src/app/services/core/error-message.service.ts
+++ EcommerceAppInterface/src/app/services/core/error-message.service.ts
@@ -9,46 +9,67 @@ import {ErrorMessages} from "../../utilities/constants/errorMessages";
 export class ErrorMessageService {
   isFormValidate:boolean = false;
   _error:FormErrorMessage[] = [];
-  _errorMessageList:any[] = [];
+  // _errorMessageList:any[] = [];
+  // errorMessage:string = "";
 
   //filter error messages from errorMessages list and return global error message list
   private generateErrorMessage(errorMessages: FormErrorMessage[]){
-    this._errorMessageList = [...errorMessages.map((errorMessage: FormErrorMessage) => {
+    return errorMessages.map((errorMessage: FormErrorMessage) => {
       return ErrorMessages.errorMessages.find((error) => {
         return error.controlName === errorMessage.name && error.errorName === errorMessage.error;
       });
-    })];
+    });
   }
 
   //get form validation and return error messages
   public checkFormValidation(formGroup: FormGroup){
     this._error = [];
+    formGroup.invalid ? this.isFormValidate = false : this.isFormValidate = true;
     Object.keys(formGroup.controls).forEach(field => {
     const control = formGroup?.get(field)!;
     //check if control has errors
       if(control.errors){
-        this.isFormValidate = false;
+        console.log(control.errors)
+        console.log(field)
         Object.keys(control.errors).forEach(keyError => {
           this._error.push({
             name: field,
             error: keyError,
           });
         });
-        //filter error messages from global errorMessages list
-        this.generateErrorMessage(this._error);
-      }else{
-        this.isFormValidate = true;
+        this.isFormValidate = false;
       }
     });
+  }
+   passwordMatchValidator(control: FormGroup) {
+    const password = control?.get('password')!.value;
+    const confirmPassword = control?.get('confirmPassword')!.value;
+    if(password != null && confirmPassword != null){
+      if (password !== confirmPassword) {
+        control?.get('confirmPassword')!.setErrors({ notMatch: true }); // Set the error explicitly for confirmPassword control
+      } else {
+        control?.get('confirmPassword')!.setErrors(null); // Clear the error if passwords match
+      }
+    }
+    return null;
+  }
 
-    console.log(this.error('email'));
-    console.log(this.error('password'));
+  private confirmPasswordValidation(formGroup: FormGroup){
+    console.log('asdasd')
+    if(formGroup.get("password")?.value != formGroup.get("confirmPassword")?.value){
+      this._error.push({
+        name: "confirmPassword",
+        error: "notMatch",
+      });
+      this.isFormValidate = false;
+      console.log(this._error)
+    }
   }
 
   //return error message from filtered global error message list by control name
-  public error(control:string) {
-    return this._errorMessageList.find((error) => {
-      return error.controlName === control;
+  public error(control:string){
+    return this.generateErrorMessage(this._error).find((error) => {
+      return error?.controlName === control;
     })?.message;
   }
 }
