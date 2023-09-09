dfc4872bdfab1730c413a2a5df04b58af6e030bc
jaffarabbas <gamaportal8@gmail.com>
2023-09-10 01:34:30 +0500

    Commited File : register.component.ts at 10/09/2023 1:34:30 am

--- EcommerceAppInterface/src/app/modules/auth/components/register/register.component.ts
+++ EcommerceAppInterface/src/app/modules/auth/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ErrorMessageService} from "../../../../services/core/error-message.service";
+import {DropdownItems} from "../../../../interfaces/dropDownItems";
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,42 @@ import { Component, Input } from '@angular/core';
 })
 export class RegisterComponent {
   @Input() isShowBanner: boolean = true;
+
+  accType:DropdownItems[] = [
+    {id: 1, name: "Sliver"},
+    {id: 2, name: "Gold"},
+    {id: 3, name: "Platinum"},
+  ];
+  constructor(public errorMessageService:ErrorMessageService) {
+  }
+
+  formGroup: FormGroup = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    firstName: new FormControl('', [
+      Validators.required,
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+    ]),
+    accountType : new FormControl('', [
+      Validators.required,
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    confirmPassword: new FormControl('',[
+      Validators.required,
+    ]),
+  });
+
+  submit() {
+    this.errorMessageService.checkFormValidation(this.formGroup);
+    if(this.errorMessageService.isFormValidate){
+      console.log(this.formGroup.value);
+    }
+  }
 }
