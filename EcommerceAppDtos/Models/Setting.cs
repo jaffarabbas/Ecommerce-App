17ec05c7453dbe07975b5701dbf7a83f0733ed07
jaffarabbas <gamaportal8@gmail.com>
2023-09-19 23:46:58 +0500

    Working For Backend Dtos (Added Dtos)

---
+++ EcommerceAppDtos/Models/Setting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceAppServices.Models;
+
+[Keyless]
+[Table("SETTING")]
+public partial class Setting
+{
+    [Column("TAX", TypeName = "decimal(10, 2)")]
+    public decimal Tax { get; set; }
+}
---
+++ EcommerceAppDtos/Models/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceAppServices.Models;
+
+[Table("USERS")]
+public partial class User
+{
+    [Key]
+    [Column("uid")]
+    public int Uid { get; set; }
+
+    [Column("firstname")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Firstname { get; set; } = null!;
+
+    [Column("lastname")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Lastname { get; set; } = null!;
+
+    [Column("email")]
+    [StringLength(233)]
+    [Unicode(false)]
+    public string Email { get; set; } = null!;
+
+    [Column("password")]
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Password { get; set; } = null!;
+
+    [Column("acid")]
+    public int Acid { get; set; }
+
+    [Required]
+    [Column("u_status")]
+    public bool? UStatus { get; set; }
+
+    [Column("created_at", TypeName = "datetime")]
+    public DateTime CreatedAt { get; set; }
+
+    [ForeignKey("Acid")]
+    [InverseProperty("Users")]
+    public virtual AccountType Ac { get; set; } = null!;
+
+    [InverseProperty("UidNavigation")]
+    public virtual ICollection<UserOrder> UserOrders { get; set; } = new List<UserOrder>();
+}
